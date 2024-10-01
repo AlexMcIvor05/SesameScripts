ab8f298d091f47115e1fe333498d7bd2b5f06780
Alex McIvor <alex.mcivor28@gmail.com>
2024-10-01 16:23:14 -0400

    cleaned comments

--- sesameScript.java
+++ sesameScript.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.event.*;
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
+
 public class sesameScript {
    
     public void actionPerformed(ActionEvent e) {
@@ -18,6 +18,7 @@ public class sesameScript {
                 left.addActionListener(new ActionListener() { 
                     @Override 
                     public void actionPerformed(ActionEvent e) { 
+
                         // This code runs when the button is pressed 
                         JOptionPane.showMessageDialog(frame, "Move left"); 
                     } 
@@ -27,24 +28,23 @@ public class sesameScript {
                 right.addActionListener(new ActionListener() { 
                     @Override 
                     public void actionPerformed(ActionEvent e) { 
+
                         // This code runs when the button is pressed 
                         JOptionPane.showMessageDialog(frame, "Move right"); 
                     } 
                 }); 
 
-                 
+
+        //add buttons to frame
         frame.add(left);
-        //frame.getContentPane().add(right);
-        
         frame.add(right);
         frame.setLayout(new GridLayout());
 
 
-        //frame.setLayout(new BorderLayout(frame.getContentPane(), BoxLayout.X_AXIS));
         frame.setSize(300, 150); // Set your desired size
 		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close the application when the window is closed
-		//frame.setLocationRelativeTo(null); // Center the window on the screen
 
-		frame.setVisible(true); // Make the window visible
+        // Make the window visible
+		frame.setVisible(true); 
     }
 }
